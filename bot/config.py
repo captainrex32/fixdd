665123833bc70c67404c9a49102d06b6d9c8b134
Ashutosh15rai <115929606+Ashutosh15rai@users.noreply.github.com>
2024-01-19 09:18:47 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,18 +5,18 @@ class Config(object):
     SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
     # AHCompressBot....
     # sucks Dude
-    APP_ID = 4018758  # Updated with your API ID
-    API_HASH = "622bba3cf046315531f71f9d97fa6c2a"  # Updated with your API HASH
-    LOG_CHANNEL = "-1002108819224" # Updated with your log channel ID
+    APP_ID = get_config("APP_ID","") # Updated with your API ID
+    API_HASH = get_config("API_HASH","")  # Updated with your API HASH
+    LOG_CHANNEL = get_config("LOG_CHANNEL","") # Updated with your log channel ID
     UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
     # Get these values from my.telegram.org
-    AUTH_USERS = {6748415360}
+    AUTH_USERS = {5009250822}
     # auth users jdk 
-    TG_BOT_TOKEN = "6650630304:AAFqJ_xw_SEMydEYJJbWOmi3f89hdLJMgZ0"  # Updated with your bot token
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN","")  # Updated with your bot token
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
-    BOT_USERNAME = "Bot144255262626_bot"  # Updated with your bot username
+    BOT_USERNAME = get_config("BOT_USERNAME","")  # Updated with your bot username
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 2097152000
@@ -28,8 +28,8 @@ class Config(object):
     # maximum message length in Telegram
     MAX_MESSAGE_LENGTH = 4096
     # add config vars for the display progress
-    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "●")
-    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "○")
+    FINISHED_PROGRESS_STR = get_config("FINISHED_PROGRESS_STR", "■")
+    UN_FINISHED_PROGRESS_STR = get_config("UN_FINISHED_PROGRESS_STR", "□")
     LOG_FILE_ZZGEVC = get_config("LOG_FILE_ZZGEVC", "Log.txt")
     # because, https://t.me/c/1494623325/5603
     SHOULD_USE_BUTTONS = get_config("SHOULD_USE_BUTTONS", True)
